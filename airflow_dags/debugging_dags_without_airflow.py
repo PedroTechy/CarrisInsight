188b8868caae4b39f92b2a6b4642938f54c61ad8
Ines Lopes <inescblopes97@gmail.com>
2025-01-04 18:05:53 +0000

    Added get zip files logic and arguments.

--- airflow_dags/debugging_dags_without_airflow.py
+++ airflow_dags/debugging_dags_without_airflow.py
@@ -1,8 +1,13 @@
 import json 
 import requests
 import os
+import io
+import zipfile
+import sys
   
 BASE_API_URL = "https://api.carrismetropolitana.pt/" 
+
+
 
 
 def extract_and_store_data_test(endpoint): 
@@ -23,5 +28,44 @@ def extract_and_store_data_test(endpoint):
 
     x = 1
 
-endpoint = "municipalities"
-x = extract_and_store_data_test(endpoint)
+def extract_zip_files(file_list):
+
+    url = f"{BASE_API_URL}/gtfs"
+    response = requests.get(url)
+
+
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+    # Step 3: List the files in the zip
+        zip_file_list = zip_ref.namelist()
+        print(f"Files in the zip archive: {zip_file_list}")
+
+        for file in file_list:
+        # Step 4: Extract and parse the specific file you want
+            target_filename = file # Replace with the actual file you're interested in
+
+            if target_filename in zip_file_list:
+                with zip_ref.open(target_filename) as target_file:
+                    # Example: Read the file and parse its content
+                    file_content = target_file.read().decode('utf-8')  # Decode the bytes to string if it's a text file
+                    print(f"Content of {target_filename}:\n")
+                    print(f"Received file with len: {len(file_content)}")
+
+                with open(target_filename, 'w') as target:
+                    target.writelines(file_content)
+            else:
+                print(f"{target_filename} not found in the zip archive.")
+
+
+
+if __name__ == "__main__":
+    if sys.argv[1]=='json':
+        print('Getting Json endpoints')
+        endpoint = sys.argv[2]
+        x = extract_and_store_data_test(endpoint)
+    elif sys.argv[1]=='zip':
+        print('Getting zip files')
+        extract_zip_files(['calendar_dates.txt', 'trips.txt', 'stop_times.txt'])
+    else:
+        print('''Please specify if you want to extract json or zip content. Examples: 
+                  python3 airflow_dags/debugging_dags_without_airflow.py json municipalities
+                  python3 airflow_dags/debugging_dags_without_airflow.py zip''')
