112b4422baa1c423dbc52480029ab1c6740ebd00
Ines Lopes <inescblopes97@gmail.com>
2025-01-08 23:10:03 +0000

    Added code to test sending content from memory intead of local file

--- airflow_dags/upload_data.py
+++ airflow_dags/upload_data.py
@@ -18,40 +18,41 @@ import sys
 
 # [START storage_upload_file]
 from google.cloud import storage
+from google.api_core.exceptions import PreconditionFailed
 
-
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob_with_override(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    # The path to your file to upload
-    # source_file_name = "local/path/to/file"
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    try:
+        blob.upload_from_filename(source_file_name)
+    except PreconditionFailed as error:
+        print(error)
+        print("File already exists")
+
+    
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
+    """Uploads a file to the bucket."""
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    # Optional: set a generation-match precondition to avoid potential race conditions
-    # and data corruptions. The request to upload is aborted if the object's
-    # generation number does not match your precondition. For a destination
-    # object that does not yet exist, set the if_generation_match precondition to 0.
-    # If the destination object already exists in your bucket, set instead a
-    # generation-match precondition using its generation number.
-    generation_match_precondition = 0
-
-    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+    blob.upload_from_string(contents)
 
     print(
-        f"File {source_file_name} uploaded to {destination_blob_name}."
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
     )
 
 
-# [END storage_upload_file]
-
 if __name__ == "__main__":
-    upload_blob(
+    upload_blob_with_override(
         bucket_name=sys.argv[1],
         source_file_name=sys.argv[2],
         destination_blob_name=sys.argv[3],
