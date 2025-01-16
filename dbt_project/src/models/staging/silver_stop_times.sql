7b9d1b849bd244b9657de5c548aa14f457baaec4
Ines Lopes <inescblopes97@gmail.com>
2025-01-16 22:38:47 +0000

    Added first query to create silver stop times

---
+++ dbt_project/src/models/staging/silver_stop_times.sql
@@ -0,0 +1,29 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with filtered as (
+SELECT 
+departure_time,
+stop_sequence,
+shape_dist_traveled,
+trip_id
+           
+FROM `data_eng_project_group3.raw_stop_times`
+QUALIFY
+  ROW_NUMBER() OVER(PARTITION BY trip_id ORDER BY stop_sequence DESC) = 1
+  OR ROW_NUMBER() OVER(PARTITION BY trip_id ORDER BY stop_sequence ASC) = 1
+)
+
+select 
+LAG(departure_time, 1) OVER(PARTITION BY trip_id ORDER BY stop_sequence) AS start_time,
+departure_time,
+stop_sequence,
+shape_dist_traveled,
+trip_id
+
+from filtered
+qualify
+ROW_NUMBER() OVER(PARTITION BY trip_id ORDER BY stop_sequence DESC) = 1
