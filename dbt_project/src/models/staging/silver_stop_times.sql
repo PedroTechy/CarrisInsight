ec9b6c74b2dccfc6bedd6114bf856a08853d02c1
Daniel Ducat <moraesd346@gmail.com>
2025-01-18 17:50:36 +0000

    Changed to new dataset nomenclature

--- dbt_project/src/models/staging/silver_stop_times.sql
+++ dbt_project/src/models/staging/silver_stop_times.sql
@@ -5,24 +5,21 @@
 }}
 
 with filtered as (
-SELECT DISTINCT
+SELECT 
 departure_time,
-arrival_time,
 stop_sequence,
 shape_dist_traveled,
 trip_id
            
-FROM {{ source('raw_dataset', 'stop_times') }}
-
+FROM `data_eng_project_group3_raw.stop_times`
 QUALIFY
   ROW_NUMBER() OVER(PARTITION BY trip_id ORDER BY stop_sequence DESC) = 1
   OR ROW_NUMBER() OVER(PARTITION BY trip_id ORDER BY stop_sequence ASC) = 1
 )
 
-
 select 
 LAG(departure_time, 1) OVER(PARTITION BY trip_id ORDER BY stop_sequence) AS start_time,
-arrival_time,
+departure_time,
 stop_sequence,
 shape_dist_traveled,
 trip_id
