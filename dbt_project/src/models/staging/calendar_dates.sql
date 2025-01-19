3adda9b7a07351314ad6276b7a49b7d9ec2b2de1
Daniel Ducat <danielducatmoraes@gmail.com>
2025-01-19 20:12:36 +0000

    Added dbt test for calendar_dates

--- dbt_project/src/models/staging/calendar_dates.sql
+++ dbt_project/src/models/staging/calendar_dates.sql
@@ -3,8 +3,7 @@ SELECT
     CAST(day_type AS INT64) AS day_type,
     CAST(exception_type AS INT64) AS exception_type,
     CAST(holiday AS INT64) AS holiday,
-    CAST(period AS INT64) AS period,
-    pe.period_name AS period_name,
+    COALESCE(pe.period_name, 'Outros') AS period_name,
     CAST(service_id AS STRING) AS service_id
 FROM {{ source('raw_dataset', 'calendar_dates') }} cd
 LEFT JOIN {{ source('raw_dataset', 'periods') }} pe 
