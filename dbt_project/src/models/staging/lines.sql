b0f476fafe88368646805bc3612d7bbb54902306
Daniel Ducat <danielducatmoraes@gmail.com>
2025-01-19 16:21:24 +0000

    Fixed staging lines

--- dbt_project/src/models/staging/lines.sql
+++ dbt_project/src/models/staging/lines.sql
@@ -1,7 +1,6 @@
 SELECT
     id AS line_id,
-    long_name AS route_name,
-    line_id,
+    long_name AS line_name,
     routes AS list_routes,
     patterns AS list_patterns
 FROM {{ source('raw_dataset', 'lines') }}
