3e86505a03f332bfc32dde559de5ee4f1d46cca4
Pedro <pedrodcpereira@hotmail.com>
2025-01-19 21:13:09 +0000

    fixed dim_routes, routes and stops to cope with previous changes

--- dbt_project/src/models/staging/routes.sql
+++ dbt_project/src/models/staging/routes.sql
@@ -2,7 +2,8 @@ SELECT
     r.id AS route_id,
     r.long_name AS route_name,
     r.line_id,
-    ARRAY_AGG(m.name) AS list_municipality_name
+    STRING_AGG(m.name, ';') AS list_municipality_name,  -- concatenate municipality names with semicolons
+    STRING_AGG(CAST(m.id AS STRING), ';') AS list_municipality_ids  -- concatenate municipality IDs with semicolons
 FROM {{ source('raw_dataset', 'routes') }} r
 LEFT JOIN {{ source('raw_dataset', 'municipalities') }} m
     ON m.id IN UNNEST(r.municipalities)
