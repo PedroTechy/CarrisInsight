58ae99df93e3402ef43e742ffa39c5e0a1408603
Pedro <pedrodcpereira@hotmail.com>
2025-01-21 21:45:22 +0000

    fix fact_trips

--- dbt_project/src/models/marts/fact_trips.sql
+++ dbt_project/src/models/marts/fact_trips.sql
@@ -1,6 +1,6 @@
 WITH trip_dates AS (
     -- Get all possible dates for trips based on calendar and service_id
-    SELECT 
+    SELECT DISTINCT
         t.trip_id,
         cd.date as service_date
     FROM {{ source('staging_dataset', 'trips') }} t
@@ -8,16 +8,21 @@ WITH trip_dates AS (
         ON cd.service_id = t.service_id
 )
 
-SELECT
+SELECT DISTINCT
     st.trip_id,
     st.scheduled_start_time AS planned_start_time,
     st.scheduled_end_time AS planned_end_time,
+    td.service_date as planned_date,
     st.shape_dist_traveled AS total_distance_traveled,
     dr.sk_routes AS route_key,
-    (st.shape_dist_traveled * 0.001) / 
-    (ABS(TIME_DIFF(st.scheduled_end_time, st.scheduled_start_time, MINUTE)) / 60.0) AS average_speed_kmh,
-    t.direction_id as direction,
-    td.service_date as planned_date
+    st.shape_dist_traveled * 0.001 / 
+    (CASE 
+        WHEN st.scheduled_end_time < st.scheduled_start_time 
+        THEN (1440 - ABS(TIME_DIFF(st.scheduled_end_time, st.scheduled_start_time, MINUTE))) / 60.0 -- sub 1440 when trip ends after midnigth
+        ELSE ABS(TIME_DIFF(st.scheduled_end_time, st.scheduled_start_time, MINUTE)) / 60.0
+    END) AS average_speed_kmh,    
+    t.direction_id as direction
+
 FROM {{ source('staging_dataset', 'stop_times') }} st
 LEFT JOIN {{ source('staging_dataset', 'trips') }} t
     ON t.trip_id = st.trip_id
